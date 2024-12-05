8dbac7b30ed572c66d92e95bdd51c77e06cd266c
Jeni Williams <stanfordjeni@gmail.com>
2024-12-05 12:22:24 -0800

    Add showPage() function, page button click event handler to script.js

--- js/script.js
+++ js/script.js
@@ -10,17 +10,37 @@ const numberOfStudents = data.length
 //APPEND ELEMENTS
 const studentList = document.querySelector('.student-list')
 const linkList = document.querySelector('.link-list')
+const searchBar = document.querySelector('.searchBar')
 
 // /*
 // Create the `showPage` function
 // This function will create and insert/append the elements needed to display a "page" of nine students
 // */
-// function showPage(){
-//    /* Pseudocode
-//    - select the 
-//    */
-// }
+function showPage(pageNumber = 1) {
+   studentList.innerHTML = ``
+   // Select the index numbers for the students on that page
+   let startIndex = (pageNumber - 1) * PER_PAGE_COUNT
+   let endIndex = startIndex + PER_PAGE_COUNT
+   console.log(startIndex, endIndex)
 
+   // For loop to create and insert the individual student <li>
+   for (let i = startIndex; i < endIndex; i++) {
+      let studentInfo = data[i]
+      let studentCard = `
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${studentInfo.picture.large}" alt="Profile Picture">
+               <h3>${studentInfo.name.title}. ${studentInfo.name.first} ${studentInfo.name.last}</h3>
+               <span class="email">${studentInfo.email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${studentInfo.registered.date}</span>
+            </div>
+         </li>
+      `
+      studentList.insertAdjacentHTML('beforeend', studentCard)
+   }
+}
 
 /*
 Create the `addPagination` function
@@ -43,6 +63,16 @@ function addPagination() {
    }
 }
 
+/* Page Button Click Event Handler Function */
+linkList.addEventListener('click', (e) => {
+   const buttonClicked = e.target
+   buttonClicked.className = 'active'
+   const buttonActive = linkList.querySelector('.active')
+   buttonActive.className = ''
+   const pageSelected = buttonClicked.textContent
+   showPage(pageSelected)
+})
+
 // Call functions
-// showPage()
+showPage()
 addPagination()
